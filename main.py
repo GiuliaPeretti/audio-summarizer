771e139daecc24f1c2be4c0ee119935caa964eea
GiuliaPeretti <giulia.peretti12@gmail.com>
2024-06-25 09:26:26 +0200

    italian works

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ import assemblyai as aai
 from groq import Groq
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
-# from transformers import pipeline
+import speech_recognition as sr
+from pydub import AudioSegment
+
+# from transformers import pipeline S
 
 def transcribe_audio(file):
     aai.settings.api_key = open('api_key.txt', 'r').read()
@@ -20,7 +23,7 @@ def summarize(text):
     )
     chat = ChatGroq(temperature=0, groq_api_key="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", model_name="mixtral-8x7b-32768")
     
-    prompt = ChatPromptTemplate.from_messages([("human", "Summarize this text: \n"+text)])
+    prompt = ChatPromptTemplate.from_messages([("human", "Riassumi questo testo: \n"+text)])
     chain = prompt | chat
     print()
     result=""
@@ -42,17 +45,30 @@ def summarize(text):
     # return(new_aliases[len(new_aliases)-10:])
     return(result)
 
+def prova(file):
 
+    recognizer_instance = sr.Recognizer() # Crea una istanza del recognizer
+    wav = sr.AudioFile(file) # formati riconosciuti: .aiff .flac .wav
+
+    with wav as source:
+        recognizer_instance.pause_threshold = 3.0
+        audio = recognizer_instance.listen(source)
+        print("Ok! sto ora elaborando il messaggio!")
+    try:
+        text = recognizer_instance.recognize_google(audio, language="it-IT")
+        return(text)
+    except Exception as e:
+        print(e)
 
 with gr.Blocks() as demo:
     gr.Markdown("# Audio to text")
     with gr.Row():
-        file_upload=gr.File(file_types=['.mp3'], type="filepath")
+        file_upload=gr.File(file_types=['audio'], type="filepath")
         text_result=gr.TextArea()
     text_summ=gr.TextArea()
     b_transcribe=gr.Button("Transcribe")
     b_summarize=gr.Button("Summarizer")
-    b_transcribe.click(fn=transcribe_audio, inputs=[file_upload], outputs=[text_result])
+    b_transcribe.click(fn=prova, inputs=[file_upload], outputs=[text_result])
     b_summarize.click(fn=summarize, inputs=[text_result], outputs=[text_summ])
 
 demo.launch(share=False)
